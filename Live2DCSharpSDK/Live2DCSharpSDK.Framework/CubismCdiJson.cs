f82d458de0b069210c87d335d164e983333aebb0
Scighost <scighost@outlook.com>
2026-03-14 17:19:03 +0800

    https://github.com/LorisYounger/Live2DCSharpSDK.WPF/commit/c77824951a68d61c7c23fb6c9905f4604fb17511

---
+++ Live2DCSharpSDK/Live2DCSharpSDK.Framework/CubismCdiJson.cs
@@ -0,0 +1,12 @@
+﻿namespace Live2DCSharpSDK.Framework;
+
+public class CubismCdiJson
+{
+    public const string Version = "Version";
+    public const string Parameters = "Parameters";
+    public const string ParameterGroups = "ParameterGroups";
+    public const string Parts = "Parts";
+    public const string Id = "Id";
+    public const string GroupId = "GroupId";
+    public const string Name = "Name";
+}
